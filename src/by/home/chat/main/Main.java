8ec3a90f7a4a3cf893319d114b5f1264f95d3f6e
Hanna Liavoshka <hannaliavoshka@gmail.com>
2019-11-10 23:32:09 +0300

    Tasks: after first launch

--- src/by/home/chat/main/Main.java
+++ src/by/home/chat/main/Main.java
@@ -6,10 +6,14 @@ import java.util.Scanner;
 
 // Для начала нужно выбрать, в каком режиме запускать программу – сервер или клиент.
 // Спрашиваем, как запускать программу, считываем букву ответа и запускаем соответствующий класс.
+// TODO: сделать так, чтобы клиент не видел своё сообщение (исключить из списка рассылки автора сообщения)
+// TODO: исправить баг - когда отключается клиент, чтобы другие клиенты и сервер могли работать дальше
+// TODO: поэксперимнтировать с компиляцией и запуском приложений с консоли
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("S(erver) / C(lient)?");
+        System.out.println("s(erver) / c(lient)?");
 
         while (true) {
             char answer = Character.toLowerCase(in.nextLine().charAt(0));
@@ -22,7 +26,7 @@ public class Main {
             } else {
                 System.out.println("Ввод неверный. Повторите попытку: " +
                         "в каком режиме запустить программу? " +
-                        "S(Server) / C(Client)?");
+                        "s(Server) / c(Client)?");
             }
         }
     }
